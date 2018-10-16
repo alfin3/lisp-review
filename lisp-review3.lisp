c455ac650db0738f0d0252c938e791ca84d79237
Alexei Finski <alexeifinski337@gmail.com>
2018-10-16 14:44:48 -0700

    Add files via upload

---
+++ lisp-review3.lisp
@@ -0,0 +1,41 @@
+;;;; Lisp review 3.
+;;;; SBCL 1.3.1
+;;;; space and time optimization, some snippets from On Lisp by Graham
+
+;;;; optimization with tail recursion and type declaration
+
+(defun triangle-no-tail-rec (n)
+  "(time (triangle-no-tail-rec 50000))"
+  (if (zerop n)
+      0
+      (+ n (triangle-no-tail-rec (- n 1)))))
+
+(defun triangle-no-types (n)
+  "(time (triangle-no-types 50000))"
+  (labels ((tri (c n)
+	     (if (zerop n)
+		 c
+		 (tri (+ n c)
+		      (- n 1)))))
+    (tri 0 n)))
+
+(defun triangle-types (n)
+  "(time (triangle-types 50000))"
+  (labels ((tri (c n)
+	     (declare (type fixnum n c))
+	     (if (zerop n)
+		 c
+		 (tri (the fixnum (+ n c))
+		      (the fixnum (- n 1))))))
+    (tri 0 n)))
+
+
+
+
+
+
+
+
+
+
+
